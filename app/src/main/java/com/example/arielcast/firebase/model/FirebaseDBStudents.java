5dd8e9f3353348202e5843fb77704133fc0d8f91
hodayasr <hodaya_s@hotmail.com>
2020-12-01 12:20:42 +0200

    firebase setting

---
+++ app/src/main/java/com/example/arielcast/firebase/model/FirebaseDBStudents.java
@@ -0,0 +1,4 @@
+package com.example.arielcast.firebase.model;
+
+public class FirebaseDBStudents {
+}
---
+++ app/src/main/java/com/example/arielcast/firebase/model/dataObject/CourseObj.java
@@ -0,0 +1,4 @@
+package com.example.arielcast.firebase.model.dataObject;
+
+public class CourseObj {
+}
