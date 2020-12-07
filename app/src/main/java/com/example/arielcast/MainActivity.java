f4f6885ddb8209aa18142fdb867dad80549c0bca
hodayasr <hodaya_s@hotmail.com>
2020-12-07 09:16:40 +0200

    student and Lecturer Activity

--- app/src/main/java/com/example/arielcast/MainActivity.java
+++ app/src/main/java/com/example/arielcast/MainActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -100,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                    startActivity(new Intent(MainActivity.this, StudentActivity.class));
                     Toast toast=Toast.makeText(MainActivity.this,
                             "Welcome!",
                             Toast.LENGTH_LONG);
