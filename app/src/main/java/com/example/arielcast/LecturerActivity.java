4a6a5935f02abdc6ef2d5a8f5c047272c2e33e60
hodayasr <hodaya_s@hotmail.com>
2020-12-09 20:17:22 +0200

    AddLectureActivity

--- app/src/main/java/com/example/arielcast/LecturerActivity.java
+++ app/src/main/java/com/example/arielcast/LecturerActivity.java
@@ -1,137 +1,34 @@
 package com.example.arielcast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
-import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.MediaController;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-import android.widget.VideoView;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+public class LecturerActivity extends AppCompatActivity {
 
-public class LecturerActivity extends AppCompatActivity{
-    private static final int PICK_VIDEO = 1;
-    VideoView videoView;
-    Button addLec;
-    ProgressBar progressBar;
-    EditText editText;
-    private Uri videoUri;
-    MediaController mediaController;
-    Lecture lecture;
-    StorageReference storageReference;
-    DatabaseReference databaseReference;
-    UploadTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        lecture = new Lecture();
-        storageReference = FirebaseStorage.getInstance().getReference("Lectures");
-        databaseReference = FirebaseDatabase.getInstance().getReference("Lectures");
-
-        videoView = findViewById(R.id.videoview_main);
-        addLec = findViewById(R.id.addLectureButton);
-        progressBar = findViewById(R.id.progressBar);
-        editText = findViewById(R.id.et_video_name);
-        mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        videoView.start();
-
-        addLec.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                UploadVideo();
+            public void onClick(View view) {
+                if(view.getId()==R.id.fab)
+                {
+                    startActivity(new Intent(LecturerActivity.this, AddLectureActivity.class));
+                }
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_VIDEO || requestCode == RESULT_OK ||
-                data != null || data.getData() != null){
-            videoUri = data.getData();
-            videoView.setVideoURI(videoUri);
-        }
-    }
-
-    public void ChooseVideo(View view) {
-        Intent intent = new Intent();
-        intent.setType("Lectures/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,PICK_VIDEO);
-    }
-
-    private String getExt(Uri uri){
-        ContentResolver contentResolver = getContentResolver();
-        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
-    }
-
-
-    public void ShowVideo(View view) {
-    }
-
-    private void UploadVideo(){
-        String videoName = editText.getText().toString();
-        String search = editText.getText().toString().toLowerCase();
-        if (videoUri != null || !TextUtils.isEmpty(videoName)){
-            progressBar.setVisibility(View.VISIBLE);
-            final StorageReference reference = storageReference.
-                    child(System.currentTimeMillis() + "." + getExt(videoUri));
-            uploadTask = reference.putFile(videoUri);
-
-            Task<Uri> urltask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                @Override
-                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                    if (!task.isSuccessful()){
-                        throw task.getException();
-                    }
-                    return reference.getDownloadUrl();
-                }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()){
-                        Uri downloadUri = task.getResult();
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Toast.makeText(LecturerActivity.this,"Data saved",
-                                Toast.LENGTH_SHORT).show();
-                        lecture.setName(videoName);
-                        lecture.setVideo_url(downloadUri.toString());
-                        lecture.setSearch(search);
-                        String i = databaseReference.push().getKey();
-                        databaseReference.child(i).setValue(lecture);
-                    } else {
-                        Toast.makeText(LecturerActivity.this,"Failed",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-        } else {
-            Toast.makeText(this,"All Fields are required",Toast.LENGTH_SHORT).show();
-        }
     }
 }
--- app/src/main/java/com/example/arielcast/MainActivity.java
+++ app/src/main/java/com/example/arielcast/MainActivity.java
@@ -2,14 +2,11 @@ package com.example.arielcast;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.LayoutDirection;
 import android.util.Patterns;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,24 +15,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.arielcast.firebase.model.FirebaseBaseModel;
-import com.example.arielcast.firebase.model.FirebaseDBLecturers;
-import com.example.arielcast.firebase.model.dataObject.LecturerObj;
-import com.example.arielcast.firebase.model.dataObject.StudentObj;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
