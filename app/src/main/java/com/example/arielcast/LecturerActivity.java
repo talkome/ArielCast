6bc9690b7eb5a40b544947bf6d008d1863292957
Suuuuha <suha.edris@gmail.com>
2020-12-11 14:21:57 +0200

    add email to new video

--- app/src/main/java/com/example/arielcast/LecturerActivity.java
+++ app/src/main/java/com/example/arielcast/LecturerActivity.java
@@ -3,8 +3,20 @@ package com.example.arielcast;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.arielcast.firebase.model.dataObject.LectureObj;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,7 +24,7 @@ import android.view.View;
 
 public class LecturerActivity extends AppCompatActivity {
 
-
+DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +32,38 @@ public class LecturerActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // get lecturer's email from MainActivity
+        Intent intent=getIntent();
+        String email=intent.getExtras().getString("Email");
+
+        //
+        myRef= FirebaseDatabase.getInstance().getReference().child("video");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot data : snapshot.getChildren())
+                {
+                    LectureObj p = data.getValue(LectureObj.class);
+                  //  lectures.add(p);
+                }
+               // allPostAdapter = new AllPostAdapter(AllPostActivity.this,0,0,posts);
+            //    lv.setAdapter(allPostAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // add lecture Button
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(view.getId()==R.id.fab)
                 {
-                    Intent intent=getIntent();
-                    String email=intent.getExtras().getString("Email");
+                    // save lecturer's email and start AddLectureActivity
                     Intent i=new  Intent(LecturerActivity.this, AddLectureActivity.class);
                     i.putExtra("Email",email);
                     startActivity(i);
