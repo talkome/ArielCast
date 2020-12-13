20e1dc01555700c297c3d70dcebbd9c898c0f20e
hodayasr <hodaya_s@hotmail.com>
2020-12-13 02:02:10 +0200

    LecturerActivity-show lecturer's videos ,Query orderBy email

--- app/src/main/java/com/example/arielcast/LecturerActivity.java
+++ app/src/main/java/com/example/arielcast/LecturerActivity.java
@@ -3,23 +3,32 @@ package com.example.arielcast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.arielcast.firebase.model.dataObject.LectureObj;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class LecturerActivity extends AppCompatActivity {
 
-DatabaseReference myRef;
+    DatabaseReference myRef;
+    ListView lecturesListView;
+    ArrayList<String> lecturesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,24 +42,37 @@ DatabaseReference myRef;
         Intent intent = getIntent();
         String email = intent.getExtras().getString("Email");
 
-        //
+        //show my lectures
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<>(LecturerActivity.this, android.R.layout.simple_list_item_1, lecturesList);
+
+        lecturesListView = findViewById(R.id.listView);
+        lecturesListView.setAdapter(myArrayAdapter);
+
         myRef = FirebaseDatabase.getInstance().getReference().child("video");
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot data : snapshot.getChildren()) {
-                    LectureObj p = data.getValue(LectureObj.class);
-//                    lectures.add(p);
-                }
-//                allPostAdapter = new AllPostAdapter(AllPostActivity.this,0,0,posts);
-//               lv.setAdapter(allPostAdapter);
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        });
+
+                    Query myOrderPostsQuery = myRef.orderByChild("lecturerEmail").equalTo(email);
+
+                    myOrderPostsQuery.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot data : snapshot.getChildren()) {
+                                String value = data.getValue(LectureObj.class).getName();
+                                lecturesList.add(value);
+                                myArrayAdapter.notifyDataSetChanged();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
 
         // add lecture Button
         FloatingActionButton fab = findViewById(R.id.fab);
