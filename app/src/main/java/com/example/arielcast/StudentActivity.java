58e98f7399120a4d6b46d258e4a80554a0f11456
hodayasr <hodaya_s@hotmail.com>
2020-12-16 12:40:21 +0200

    show courses in StudentActivity

--- app/src/main/java/com/example/arielcast/StudentActivity.java
+++ app/src/main/java/com/example/arielcast/StudentActivity.java
@@ -16,6 +16,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -41,15 +43,34 @@ public class StudentActivity extends AppCompatActivity {
                 "Welcome "+email+" !",
                 Toast.LENGTH_LONG).show();
 
-        mRef = FirebaseDatabase.getInstance().getReference().child("Lecturers");
+        mRef = FirebaseDatabase.getInstance().getReference().child("Courses");
 
         mRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String value = snapshot.child("courseName").getValue(String.class);
-                String value2 = snapshot.child("fullname").getValue(String.class);
-                coursesList.add(value+" - "+value2);
-                myArrayAdapter.notifyDataSetChanged();
+                String value2 = snapshot.child("lecturerId").getValue(String.class);
+                Query q=FirebaseDatabase.getInstance().getReference().child("Lecturers").child("").orderByChild("lecturerId").equalTo(value2);
+           //     coursesList.add(value+" - "+value2);
+           //     myArrayAdapter.notifyDataSetChanged();
+
+                q.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot data : snapshot.getChildren()) {
+                            String name = data.child("fullname").getValue(String.class);
+                            coursesList.add(value+" - "+name);
+                            myArrayAdapter.notifyDataSetChanged();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
 
             @Override
