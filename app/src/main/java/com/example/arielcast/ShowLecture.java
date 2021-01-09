aa8d4f304d11ef6facced38c7d3ff949dfb9818f
hodayasr <hodaya_s@hotmail.com>
2021-01-09 18:43:26 +0200

    get lecture name on ShowLecture

--- app/src/main/java/com/example/arielcast/ShowLecture.java
+++ app/src/main/java/com/example/arielcast/ShowLecture.java
@@ -2,13 +2,24 @@ package com.example.arielcast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ShowLecture extends AppCompatActivity {
+    String lectureID;
+    EditText lecture_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_lecture);
+
+        Intent i=getIntent();
+        lecture_name=findViewById(R.id.et_video_name);
+        String name=i.getExtras().getString("lecID");
+        lecture_name.setText(name);
+
+
     }
 }
